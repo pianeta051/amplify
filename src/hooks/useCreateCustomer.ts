56be7c7baba8385a1746982c3a5850b34ecb2852
Carlos <pianeta05@hotmail.com>
2024-02-01 16:04:18 +0000

    refactor customer creation

--- src/hooks/useCreateCustomer.ts
+++ src/hooks/useCreateCustomer.ts
@@ -2,8 +2,10 @@ import useSWRMutation from "swr/mutation";
 import { Customer, createCustomer } from "../services/customers";
 import { CustomerFormValues } from "../components/CustomerForm/CustomerForm";
 import { extractErrorCode } from "../services/error";
+import { useSWRConfig } from "swr";
 
 export const useCreateCustomer = () => {
+  const { mutate } = useSWRConfig();
   const { trigger, isMutating, error } = useSWRMutation<
     Customer,
     Error,
@@ -15,7 +17,16 @@ export const useCreateCustomer = () => {
     async (
       _operation,
       { arg: formValues } // Recibe primero el array de la clave y luego un objeto con los argumentos
-    ) => createCustomer(formValues), // Funcion del servicio,
+    ) => {
+      const customer = await createCustomer(formValues);
+      // Ahora que sabemos el id, modificamos la cache [customer, id]
+      await mutate<readonly [string, string], Customer>(
+        ["customer", customer.id],
+        customer,
+        { populateCache: true, revalidate: false }
+      );
+      return customer;
+    },
     {
       revalidate: false,
       populateCache: false,
