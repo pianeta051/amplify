11d728bd5bd2d06e542a2d6c97bfb454e0095209
Carlos <pianeta05@hotmail.com>
2025-04-03 16:25:40 +0100

    lint

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,67 +1,67 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "AdminQueries": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "dataapi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "exercisesauth": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    },
-    "userPoolGroups": {
-      "AdminGroupRole": "string"
-    }
-  },
-  "function": {
-    "AdminQueries3903e60d": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "dataapi": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "exercisesauthCustomMessage": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "storage": {
-    "exercises": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "SortKeyName": "string",
-      "SortKeyType": "string",
-      "StreamArn": "string"
-    },
-    "s33ca9c572": {
-      "BucketName": "string",
-      "Region": "string"
-    }
-  }
-}
+  api: {
+    AdminQueries: {
+      ApiId: "string";
+      ApiName: "string";
+      RootUrl: "string";
+    };
+    dataapi: {
+      ApiId: "string";
+      ApiName: "string";
+      RootUrl: "string";
+    };
+  };
+  auth: {
+    exercisesauth: {
+      AppClientID: "string";
+      AppClientIDWeb: "string";
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      UserPoolArn: "string";
+      UserPoolId: "string";
+      UserPoolName: "string";
+    };
+    userPoolGroups: {
+      AdminGroupRole: "string";
+    };
+  };
+  function: {
+    AdminQueries3903e60d: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    dataapi: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+    exercisesauthCustomMessage: {
+      Arn: "string";
+      LambdaExecutionRole: "string";
+      LambdaExecutionRoleArn: "string";
+      Name: "string";
+      Region: "string";
+    };
+  };
+  storage: {
+    exercises: {
+      Arn: "string";
+      Name: "string";
+      PartitionKeyName: "string";
+      PartitionKeyType: "string";
+      Region: "string";
+      SortKeyName: "string";
+      SortKeyType: "string";
+      StreamArn: "string";
+    };
+    s33ca9c572: {
+      BucketName: "string";
+      Region: "string";
+    };
+  };
+};
